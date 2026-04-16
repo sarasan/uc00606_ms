3faddbc1f9858e57834522fef297c5a33214bb62
sara <sarahkungfu32@gmail.com>
2026-04-16 23:03:27 +0100

    upcs2

---
+++ manipulacaoStrings/AlunoMnipulacaoStrings.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace ExerciciosAula02;
+
+
+public class AlunoManipulacaoStrings
+  {
+    public void Executar()
+    {
+      Console.WriteLine("\n\n EXERCÍCIOS DE MANIPULAÇÃO DE STRINGS");
+      Console.WriteLine("==========================================\n");
+
+      Console.WriteLine("===============================================");
+
+      Console.WriteLine("EXERCÍCIO 1: Contar vogais");
+      
+      Console.WriteLine("===============================================");
+
+      Console.WriteLine("Insira um a frase");
+
+      string frase = Console.ReadLine();
+
+      string vogais="aeiouAEIOU";
+    
+      //string caract;
+
+      int count = 0;
+
+      /*foreach(char caract in frase)
+        {
+
+         if (caract in vogais)
+          Console.WriteLine("");  
+        }
+        {
+            
+
+        }*/
+
+
+
+
+    }
+
+  }
--- manipulacaoStrings/obj/Debug/net10.0/manipulacaoStrings.AssemblyInfo.cs
+++ manipulacaoStrings/obj/Debug/net10.0/manipulacaoStrings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("manipulacaoStrings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab9fb9f8d6c227072f37b51b08c6d5c216e9cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e66660daa2d47fba7fdb23a9f24e0c0bf82c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("manipulacaoStrings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("manipulacaoStrings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
