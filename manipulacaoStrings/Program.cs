a3e66660daa2d47fba7fdb23a9f24e0c0bf82c15
sara <sarahkungfu32@gmail.com>
2026-04-16 22:14:42 +0100

    updtexsmps

---
+++ manipulacaoStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ExerciciosAula02;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        
+          Console.WriteLine("Fundamentos de Programação C#\n");
+    }
+
+}
